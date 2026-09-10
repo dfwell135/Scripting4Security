dc991e161bc0f57cd72734bda6e72a5a703dd1f2
dfwell135 <dfwells135@gmail.com>
2026-09-10 14:20:43 -0500

    pyramid lab

---
+++ lab3.2.14_pyramid.py
@@ -0,0 +1,15 @@
+blocks = int(input("Enter the number of blocks: "))
+
+#
+# Write your code here.
+height = 0
+layer = 1
+
+while blocks >= layer:
+    blocks -= layer
+    height = layer
+    layer += 1
+
+
+print("The height of the pyramid:", height)
+
